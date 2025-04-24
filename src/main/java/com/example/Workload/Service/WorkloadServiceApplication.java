5225fb2eb2ca1073eee94a40862eb908992d30fd
rich27a <freeazul25@outlook.es>
2025-04-24 11:26:37 -0600

    first commit

---
+++ src/main/java/com/example/Workload/Service/WorkloadServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Workload.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkloadServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkloadServiceApplication.class, args);
+	}
+
+}
