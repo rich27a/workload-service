1b7d1733234adce4b38eab0308457a667f5cdff4
rich27a <freeazul25@outlook.es>
2025-05-06 11:15:53 -0600

    WorkloadConsumer and workloadMessage added

---
+++ src/main/java/com/example/Workload/Service/messaging/jms/model/WorkloadMessage.java
@@ -0,0 +1,30 @@
+package com.example.Workload.Service.messaging.jms.model;
+
+
+
+import com.example.Workload.Service.models.ActionType;
+import com.example.Workload.Service.models.WorkloadData;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WorkloadMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String trainerUsername;
+    private String trainerFirstName;
+    private String trainerLastName;
+    private boolean isActive;
+    private LocalDate trainingDate;
+    private int trainingDuration;
+    private ActionType actionType;
+    private String authToken;
+}
