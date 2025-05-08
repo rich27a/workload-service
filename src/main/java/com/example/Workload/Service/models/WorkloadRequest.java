e6f631cb5ce8a20fa2ddc0cb4842a799899c425b
rich27a <freeazul25@outlook.es>
2025-05-08 13:00:45 -0600

    use records instead of lombok

--- src/main/java/com/example/Workload/Service/models/WorkloadRequest.java
+++ src/main/java/com/example/Workload/Service/models/WorkloadRequest.java
@@ -3,18 +3,46 @@ package com.example.Workload.Service.models;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.Data;
 
 import java.time.LocalDate;
 
-@Data
-public class WorkloadRequest {
-    @NotBlank private String trainerUsername;
-    @NotBlank private String trainerFirstName;
-    @NotBlank private String trainerLastName;
-    private boolean isActive;
-    @NotNull private LocalDate trainingDate;
-    @Positive private int trainingDuration;
-    @NotNull private ActionType actionType;
+public record WorkloadRequest(
+        @NotBlank String trainerUsername,
+        @NotBlank String trainerFirstName,
+        @NotBlank String trainerLastName,
+        boolean isActive,
+        @NotNull LocalDate trainingDate,
+        @Positive int trainingDuration,
+        @NotNull ActionType actionType
+) implements WorkloadData {
+    @Override
+    public String getTrainerUsername() {
+        return trainerUsername;
+    }
+
+    @Override
+    public String getTrainerFirstName() {
+        return trainerFirstName;
+    }
+
+    @Override
+    public String getTrainerLastName() {
+        return trainerLastName;
+    }
+
+    @Override
+    public LocalDate getTrainingDate() {
+        return trainingDate;
+    }
+
+    @Override
+    public int getTrainingDuration() {
+        return trainingDuration;
+    }
+
+    @Override
+    public ActionType getActionType() {
+        return actionType;
+    }
 }
 
