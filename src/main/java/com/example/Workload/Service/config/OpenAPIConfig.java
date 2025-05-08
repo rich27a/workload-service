3992d3d037bd018a0ae4f1c43ae70f37b9e0a50e
rich27a <freeazul25@outlook.es>
2025-05-08 14:03:29 -0600

    adding openapig config class

---
+++ src/main/java/com/example/Workload/Service/config/OpenAPIConfig.java
@@ -0,0 +1,28 @@
+package com.example.Workload.Service.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Workload Service API")
+                        .description("API to manage trainer workload information")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Workload Project")
+                                .email("test@example.com")
+                                .url("https://www.example.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
